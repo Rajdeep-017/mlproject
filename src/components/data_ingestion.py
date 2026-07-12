2e712f630ed30300d8623899309173dada92ffe4
Rajdeep <rajdeepsawant919@gmail.com>
2026-07-12 19:27:15 +0530

    'modeltrainer'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig 
+from src.components.model_trainer import modeltrainerconfig
+from src.components.model_trainer import modeltrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -45,5 +48,11 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=modeltrainer()
+    print("r2_score",modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
 
